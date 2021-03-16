b967c91ef45dfd78f78f7c5ff3ad15350566164c
Tim Porteous <tim.porteous@jagex.com>
2021-03-16 17:11:14 +0000

    I forgot to hit save before I pushed oops

--- CircusGroupsBot/Config.cs
+++ CircusGroupsBot/Config.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace CircusGroupsBot
 {
-    class Globals
+    public static class Config
     {
+        public const string DB_PASSWORD_ENV_VAR = "dbpass";
+        public const string BOT_TOKEN_ENV_VAR = "circusBotToken";
     }
 }
