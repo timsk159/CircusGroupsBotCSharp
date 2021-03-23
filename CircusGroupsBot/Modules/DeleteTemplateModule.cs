925d43c9994c344f25a018b04698aa40d545c6bb
Tim Porteous <--global>
2021-03-23 19:44:48 +0000

    Added delete and list template commands

---
+++ CircusGroupsBot/Modules/DeleteTemplateModule.cs
@@ -0,0 +1,38 @@
+﻿using CircusGroupsBot.Services;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusGroupsBot.Modules
+{
+    public class DeleteTemplateModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly CircusDbContext DbContext;
+        private readonly Logger Logger;
+
+        public DeleteTemplateModule(CircusDbContext dbContext, Logger logger)
+        {
+            this.DbContext = dbContext;
+            this.Logger = logger;
+        }
+
+        [Command("deletetemplate")]
+        [Summary("Delete a pre-created template")]
+        public Task RunModule(string templateName)
+        {
+            var template = DbContext.Templates.FirstOrDefault(e => e.TemplateName == templateName);
+            if (template == null)
+            {
+                return ReplyAsync($"No template found with name {templateName}");
+            }
+
+            DbContext.Templates.Remove(template);
+            DbContext.SaveChanges();
+
+            return ReplyAsync($"Deleted template with name {templateName}");
+        }
+    }
+}
